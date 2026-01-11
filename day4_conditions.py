4130a71a13251dbe5081dda59c73f96dd78dd992
awais-dahri <ccc924798@gmail.com>
2026-01-11 21:13:16 +0500

    Day 4: Practiced Python conditional statements

---
+++ day4_conditions.py
@@ -0,0 +1,11 @@
+# Day 4: Conditional statements in Python
+# This program checks age category using if/elif/else
+
+age = 17   # You can change this value to test
+
+if age < 18:
+    print("Minor")
+elif age <= 60:
+    print("Adult")
+else:
+    print("Senior")
