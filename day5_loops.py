b854015b3ac7ce1bc2b7bf56fcee7e2d20a6c778
awais-dahri <ccc924798@gmail.com>
2026-01-12 19:00:55 +0500

    Day 5: Practiced Python loops and basic logic

---
+++ day5_loops.py
@@ -0,0 +1,11 @@
+# Day 5: Using loops in Python
+# This program prints numbers from 1 to 5
+
+for number in range(1, 6):
+    print(number)
+
+# Print even numbers from 1 to 10
+
+for num in range(1, 11):
+    if num % 2 == 0:
+        print(num, "is even")
