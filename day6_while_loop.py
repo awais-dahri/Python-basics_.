54bfb7f2c575c0b277bf5feaa12cd3facacd92c2
awais-dahri <ccc924798@gmail.com>
2026-01-13 17:56:26 +0500

    Day 6: Practiced while loops and mini challenge

---
+++ day6_while_loop.py
@@ -0,0 +1,17 @@
+# Day 6: While loop practice
+# This program prints numbers from 1 to 5
+
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+# Mini challenge:
+# Print numbers from 10 down to 1
+
+number = 10
+
+while number >= 1:
+    print(number)
+    number -= 1
