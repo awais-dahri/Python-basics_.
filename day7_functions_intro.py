82cb1edae089d73de38d0b814175630ac428b898
awais-dahri <ccc924798@gmail.com>
2026-01-14 20:50:38 +0500

    Day 7: Introduction to Python functions

---
+++ day7_functions_intro.py
@@ -0,0 +1,14 @@
+# Day 7: Introduction to functions
+# Learning how to define and call functions in Python
+
+def greet():
+    print("Hello! This is my first Python function.")
+
+greet()
+
+# Optional step: function with a parameter
+
+def greet_person(name):
+    print("Hello", name)
+
+greet_person("Awais")
